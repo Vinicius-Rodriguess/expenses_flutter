b3c20d8449921c951459018620cc20c3efe1f32a
Vinicius-Rodriguess <rodrigues.vini.2004@gmail.com>
2025-06-23 20:16:34 -0300

    :sparkles: feat: Adicionado Componente Transaction_user

---
+++ lib/components/transaction_user.dart
@@ -0,0 +1,33 @@
+import 'package:expenses_flutter/components/transaction_form.dart';
+import 'package:expenses_flutter/components/transaction_list.dart';
+import 'package:expenses_flutter/models/transaction.dart';
+import 'package:flutter/material.dart';
+
+class TransactionUser extends StatefulWidget {
+  @override
+  State<TransactionUser> createState() => _TransactionUserState();
+}
+
+class _TransactionUserState extends State<TransactionUser> {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tenis de Corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [TransactionList(_transactions), TransactionForm()],
+    );
+  }
+}
