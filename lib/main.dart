64f39ae0066770a013255ce1c3f663640f8534bc
Vinicius-Rodriguess <rodrigues.vini.2004@gmail.com>
2025-06-23 20:18:29 -0300

    :recycle: refactor: Componentizado Transaction_user

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:expenses_flutter/transaction.dart';
+import 'package:expenses_flutter/components/transaction_user.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(ExpensesApp());
@@ -11,27 +11,11 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final _transactions = [
-    Transaction(
-      id: 't1',
-      title: 'Novo Tenis de Corrida',
-      value: 310.76,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Conta de luz',
-      value: 211.30,
-      date: DateTime.now(),
-    ),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Despesas Pessoais')),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
@@ -42,11 +26,7 @@ class MyHomePage extends StatelessWidget {
               child: Text('Grafico'),
             ),
           ),
-          Column(
-            children: _transactions.map((tr) {
-              return Card(child: Text(tr.title));
-            }).toList(),
-          ),
+          TransactionUser(),
         ],
       ),
     );
